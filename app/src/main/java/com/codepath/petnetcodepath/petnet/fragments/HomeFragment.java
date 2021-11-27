f3ea7612b51eecec3eb57ddb1f393a6255f347bf
John Royal <34844819+john-royal@users.noreply.github.com>
2021-11-26 20:00:37 -0500

    feat: move some options from home fragment to settings fragment

--- app/src/main/java/com/codepath/petnetcodepath/petnet/fragments/HomeFragment.java
+++ app/src/main/java/com/codepath/petnetcodepath/petnet/fragments/HomeFragment.java
@@ -1,32 +1,20 @@
 package com.codepath.petnetcodepath.petnet.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.codepath.petnetcodepath.petnet.activities.LoginActivity;
-import com.codepath.petnetcodepath.petnet.activities.PetProfileActivity;
 import com.codepath.petnetcodepath.petnet.R;
-import com.codepath.petnetcodepath.petnet.activities.UserProfileActivity;
-import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
-    Button btnSignout;
-    Button btnuserprofile;
-    Button btnpetprofile;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -39,59 +27,8 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
-    private void goToLoginActivity() {
-        //using intent
-        Intent i = new Intent(getContext(), LoginActivity.class);
-        startActivity(i);
-        getActivity().finish(); // finish the main activity once the log out succeeded
-    }
-
-    private void signoutUser() {
-        ParseUser.logOut();
-        ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
-    }
-
-
-    private void goToUserProfileActivity() {
-        //using intent
-        Intent i = new Intent(getContext(), UserProfileActivity.class);
-        startActivity(i);
-    }
-
-    private void goToPetProfileActivity() {
-        //using intent
-        Intent i = new Intent(getContext(), PetProfileActivity.class);
-        startActivity(i);
-    }
-
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        btnSignout = view.findViewById(R.id.btnSignout);
-        btnuserprofile = view.findViewById(R.id.btnuserprofile);
-        btnpetprofile = view.findViewById(R.id.btnpetprofile);
-        btnuserprofile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToUserProfileActivity();
-            }
-        });
-
-        btnpetprofile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToPetProfileActivity();
-            }
-        });
-
-        btnSignout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signoutUser();
-                Toast.makeText(getContext(), "Successfully Logged Out!!", Toast.LENGTH_SHORT).show();
-                goToLoginActivity();
-            }
-        });
     }
 }
--- app/src/main/java/com/codepath/petnetcodepath/petnet/fragments/SettingsFragment.java
+++ app/src/main/java/com/codepath/petnetcodepath/petnet/fragments/SettingsFragment.java
@@ -1,23 +1,33 @@
 package com.codepath.petnetcodepath.petnet.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.codepath.petnetcodepath.petnet.R;
+import com.codepath.petnetcodepath.petnet.activities.LoginActivity;
+import com.codepath.petnetcodepath.petnet.activities.PetProfileActivity;
+import com.codepath.petnetcodepath.petnet.activities.UserProfileActivity;
+import com.parse.ParseUser;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
+
+    Button btnUserProfile;
+    Button btnPetProfile;
+    Button btnSignOut;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -34,7 +44,33 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        btnUserProfile = view.findViewById(R.id.btnUserProfile);
+        btnPetProfile = view.findViewById(R.id.btnPetProfile);
+        btnSignOut = view.findViewById(R.id.btnSignOut);
 
+        btnUserProfile.setOnClickListener(l -> {
+            goTo(UserProfileActivity.class);
+        });
+        btnPetProfile.setOnClickListener(l -> {
+            goTo(PetProfileActivity.class);
+        });
+        btnSignOut.setOnClickListener(l -> {
+            signOut();
+        });
     }
 
+    private void goTo(Class<?> activityClass) {
+        Intent i = new Intent(getContext(), activityClass);
+        startActivity(i);
+    }
+
+    private void signOut() {
+        ParseUser.logOut();
+
+        Intent i = new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
+
+        Toast.makeText(getContext(), "You’ve been signed out", Toast.LENGTH_SHORT).show();
+        getActivity().finish();
+    }
 }
